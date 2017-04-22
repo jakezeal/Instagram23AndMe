275888c9627be636ca54e50cc10f7773ae8a2c0d
Jake Zeal <jake@jakezeal.com>
2017-04-22 12:09:50 -0700

    add frameworks to project

---
+++ Instagram23AndMe/Networking/APIManager.swift
@@ -0,0 +1,14 @@
+//
+//  APIManager.swift
+//  Instagram23AndMe
+//
+//  Created by Jake on 4/22/17.
+//  Copyright © 2017 Jake. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+final class APIManager {
+    
+}
